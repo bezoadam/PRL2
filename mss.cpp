12afc18dddfcf827e61bf22395402c8c1b9bd5fc
Adam Bezak <bezoadam95@gmail.com>
2018-03-30 18:47:53 +0200

    scatter

--- mss.cpp
+++ mss.cpp
@@ -16,14 +16,14 @@ void printVector(vector<int> vec) {
 }
 
 int main(int argc, char ** argv) {
-	int numprocs;
+	int numprocs, myId;
 	vector<int> values;
 	cout << "test";
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);       // zjistíme, kolik procesů běží
+	MPI_Comm_rank(MPI_COMM_WORLD, &myId);           // zjistíme id svého procesu
 
-// cteni vygenerovanych cisel
 	char input[]= "numbers";                          //jmeno souboru
 	int number;                                     //hodnota pri nacitani souboru
 	fstream fin;                                    //cteni ze souboru
@@ -38,7 +38,15 @@ int main(int argc, char ** argv) {
 
 	}
 	fin.close();
-	printVector(values);
+	// printVector(values);
+
+	int size = values.size() / numprocs;
+	int *sub_array = (int *) malloc(size * sizeof(int));
+	int *original_array = &values[0];
+	MPI_Scatter(original_array, size, MPI_INT, sub_array, size, MPI_INT, 0, MPI_COMM_WORLD);
+
+	vector<int> finalValues(sub_array, sub_array + size);
+	printVector(finalValues);
 
 	MPI_Finalize();
 	return 0;
